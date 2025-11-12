7a97874b55a3faf56393b9f05f2ad5a8e766aa12
luanrichardsz <luanrchardof@gmail.com>
2025-11-12 13:37:57 -0300

    Criei a classe ConnectionBD no pacote db com a conexão executando com exito!

---
+++ projeto/src/bd/ConnectionBD.java
@@ -0,0 +1,20 @@
+package bd;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionBD {
+    public static void main(String[] args) {
+        try {
+            Connection connection = DriverManager
+                    .getConnection("jdbc:postgresql://localhost:5432/byte_bank", "postgres", "1234");
+
+            System.out.println("Conexão com BD com sucesso!");
+
+            connection.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
